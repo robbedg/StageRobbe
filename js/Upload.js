3b3a138fbbf668e8b60f8692e43e618fc781d655
Robbe De Geyndt <robbe.degeyndt@student.odisee.be>
2017-02-20 16:00:08 +0100

    Cleaner exiting by failed upload.

--- js/Upload.js
+++ js/Upload.js
@@ -6,13 +6,13 @@ $(document).ready(function(){
 
 	$("#dropzone").dropzone({
 		paramName: 'file',
-		maxFilesize: 2,
+		maxFilesize: 2, //MB
 		maxFiles: 1,
 		addRemoveLinks: true,
 		acceptedFiles: 'image/*',
 		dictDefaultMessage: 'Drop image here to upload.<br/>Or click to browse.',
 		success: function($file, $response) {
-			this.removeFile($file);
+			this.removeAllFiles();
 			$(".modal").modal('toggle');
 			$(".modal").modal('hide');
 			$("#successdiv").removeClass('hidden');
