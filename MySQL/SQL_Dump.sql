9107ffcfe927a9975f8439b9e9a42d5f3876714b
Robbe De Geyndt <robbe.degeyndt@student.odisee.be>
2017-02-22 15:09:05 +0100

    Update to SQL_Dump.sql

--- MySQL/SQL_Dump.sql
+++ MySQL/SQL_Dump.sql
@@ -1,4 +1,4 @@
-DROP TABLE locations;
+DROP TABLE IF EXISTS locations;
 
 CREATE TABLE `locations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -7,7 +7,7 @@ CREATE TABLE `locations` (
   UNIQUE KEY `naam_UNIQUE` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
-DROP TABLE categories;
+DROP TABLE IF EXISTS categories;
 
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -17,7 +17,7 @@ CREATE TABLE `categories` (
   UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
-DROP TABLE items;
+DROP TABLE IF EXISTS items;
 
 CREATE TABLE `items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -33,18 +33,29 @@ CREATE TABLE `items` (
   CONSTRAINT `location_id` FOREIGN KEY (`location_id`) REFERENCES `locations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
 
-DROP TABLE users;
+DROP TABLE IF EXISTS roles;
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` varchar(8) NOT NULL,
   `firstname` varchar(45) NOT NULL,
   `lastname` varchar(45) NOT NULL,
+  `role_id` int(11) NOT NULL DEFAULT '1',
   `password` varchar(60) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `role_id` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
-DROP TABLE usernotes;
+DROP TABLE IF EXISTS usernotes;
 
 CREATE TABLE `usernotes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -55,7 +66,7 @@ CREATE TABLE `usernotes` (
   PRIMARY KEY (`id`),
   KEY `item_id_idx` (`item_id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `item_id` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `item_id` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=latin1;
 
