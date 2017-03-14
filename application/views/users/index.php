7e27d4988fc2736a14c5349dafe81b44336d241d
Robbe De Geyndt <robbe.degeyndt@student.odisee.be>
2017-03-14 16:55:45 +0100

    Added some structure to profile page. Issue #29

--- application/views/users/index.php
+++ application/views/users/index.php
@@ -1,2 +1,16 @@
 <!-- title -->
 <h2>Profile</h2>
+
+<!-- hidden fields -->
+<input type="hidden" id="user_id" value="<?=$_SESSION['id']?>">
+
+<!-- info -->
+<div id="info">
+    <h3>Info</h3>
+
+</div>
+
+<div id="active-loans">
+    <h3>Items in use</h3>
+
+</div>
